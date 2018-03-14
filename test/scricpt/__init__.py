447d395a63357645a7f2aac4666af87eb0c6c4ec
liuyang <liuyang@wind-mobi.com>
2018-03-14 10:26:57 +0800

    init repository

---
+++ test/scricpt/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+# alarm_clock.py
+
+# Description: A simple Python program to make the computer act
+# like an alarm clock. Start it running from the command line
+# with a command line argument specifying the duration in minutes
+# after which to sound the alarm. It will sleep for that long,
+# and then beep a few times. Use a duration of 0 to test the
+# alarm immediiately, e.g. for checking that the volume is okay.
+
+# Author: Vasudev Ram - http://www.dancingbison.com
+
+import sys
+import string
+from time import sleep
+
+sys.stdout.write('abc')
+sleep(10)
